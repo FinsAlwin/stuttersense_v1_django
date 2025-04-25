78f4420121ded72f084396afc38b89ac17695b62
Alwin <alwinwk101@live.com>
2025-04-25 10:40:29 +0530

    updated 16

--- stuttersense_v1/settings.py
+++ stuttersense_v1/settings.py
@@ -183,6 +183,7 @@ CORS_ALLOW_ALL_ORIGINS = False  # Don't allow all origins for security
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://stuttersense.rnd.parel.co"
 ]
 
 # Additional CORS settings
